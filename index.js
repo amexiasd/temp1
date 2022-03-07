af9c3a3e1889d965f312a0871d9714594d8870bc
devamex <85012779+devamex@users.noreply.github.com>
2022-03-07 13:06:27 +0200

    sliding menu

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click',()=>{
+    burger.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+
+burger.addEventListener('click',() => {
+   
+});
 console.log("Template 1 Test");
